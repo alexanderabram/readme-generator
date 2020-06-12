1cea2f3e86ee75d91171b7a0223cbc9cde2d15d0
AMA <alexanderabram6440@gmail.com>
2020-06-12 18:41:55 -0500

    created contributing question

--- index.js
+++ index.js
@@ -7,32 +7,38 @@ const questions = [
         name: 'title',
         type: 'input',
         message: 'What is the project Title?'
-    }
+    },
     {
         name: 'description',
         type: 'input',
         message: 'Provide a description outlining your project'
-    }
+    },
     {
         name: 'tableOfContents',
         type: 'input',
         message: 'Outline the README table of contents here'
-    }
+    },
     {
         name: 'installation',
         type: 'input',
         message: 'Provide installation instructions here'
-    }
+    },
     {
         name: 'usage',
         type: 'input',
         message: 'How is your application used? Provide instructions here'
-    }
+    },
     {
         name: 'license',
         type: 'input',
         message: 'Would you like to use a license? If yes, provide the license type. If no, type none'
-    }
+    },
+    {
+        name: 'contributing',
+        type: 'input',
+        message: 'Provide instructions for contributing here'
+    },
+    
 ];
 
 // function to write README file
