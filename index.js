78dd6b43f90b50a232e32d2730a5342529590f86
AMA <alexanderabram6440@gmail.com>
2020-06-13 13:32:33 -0500

    working on fixing write file function

--- index.js
+++ index.js
@@ -53,8 +53,9 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-    resturn fs.writeFileSync(path.join(process.cwd(),fileName),data);
-} 
+    return fs.writeFileSync(path.join(process.cwd(),fileName), data)
+}
+
 
 // function to initialize program
 function init() {
