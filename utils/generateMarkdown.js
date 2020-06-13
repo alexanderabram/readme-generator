bfe5ba2e43bbd8ba621efcc9e600b14df1ed5696
AMA <alexanderabram6440@gmail.com>
2020-06-13 14:07:16 -0500

    working on markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,9 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}`;
-    return `# ${data.description}`;
+    return `# ${data.title}
+
+  `;
+    
 }
 
 module.exports = generateMarkdown;
