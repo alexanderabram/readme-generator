7d8bdb9689676ada5c99fc216a09359eb0d30230
AMA <alexanderabram6440@gmail.com>
2020-06-13 13:31:37 -0500

    fixed typo in const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer'); 
 var fs = require('fs');
 const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('generateMarkdown');
 const questions = [
 
     {
